04c1d5ca0999474d59e426a79ea69cdf06b91bb6
RogerioGomes29 <rogeriogomespoa1803@gmail.com>
2024-02-27 19:25:29 -0300

    Acesso ao menu de dados cadastrados

--- src/Main.java
+++ src/Main.java
@@ -18,62 +18,71 @@ public class Main {
             System.out.println("0 - Sair");
             System.out.println("Digite aqui a opção");
             opcao = scanner.nextInt();
-           switch (opcao){
-               case 1:
-                   System.out.println(":::::::::::::::::::::Bem vindo ao menu funcionario:::::::::::::::::::::");
+            switch (opcao) {
+                case 1:
+                    System.out.println(":::::::::::::::::::::Bem vindo ao menu funcionario:::::::::::::::::::::");
 
-                   System.out.println("Digite seu nome:");
-                   locacao.getFuncionario().setNome(scanner.next());
-                   System.out.println("Digite seu cpf:");
-                   locacao.getFuncionario().setCpf(scanner.nextLong());
-                   System.out.println("Digite seu cargo:");
-                   locacao.getFuncionario().setCargo(scanner.next());
-                   System.out.println("Digite seu salario:");
-                   locacao.getFuncionario().setSalario(scanner.next());
-                   System.out.println("Digite sua rua:");
-                   locacao.getFuncionario().getEndereco().setRua(scanner.next());
-                   System.out.println("Digite seu bairro:");
-                   locacao.getFuncionario().getEndereco().setBairro(scanner.next());
-                   System.out.println("Digite o numero da sua casa:");
-                   locacao.getFuncionario().getEndereco().setNumero(scanner.nextInt());
-                   break;
-               case 2:
-                   System.out.println(":::::::::::::::::::::Bem vindo ao menu usuário:::::::::::::::::::::");
+                    System.out.println("Digite seu nome:");
+                    locacao.getFuncionario().setNome(scanner.next());
+                    System.out.println("Digite seu cpf:");
+                    locacao.getFuncionario().setCpf(scanner.nextLong());
+                    System.out.println("Digite seu cargo:");
+                    locacao.getFuncionario().setCargo(scanner.next());
+                    System.out.println("Digite seu salario:");
+                    locacao.getFuncionario().setSalario(scanner.next());
+                    System.out.println("Digite sua rua:");
+                    locacao.getFuncionario().getEndereco().setRua(scanner.next());
+                    System.out.println("Digite seu bairro:");
+                    locacao.getFuncionario().getEndereco().setBairro(scanner.next());
+                    System.out.println("Digite o numero da sua casa:");
+                    locacao.getFuncionario().getEndereco().setNumero(scanner.nextInt());
+                    break;
+                case 2:
+                    System.out.println(":::::::::::::::::::::Bem vindo ao menu usuário:::::::::::::::::::::");
 
-                   System.out.println("Digite seu nome:");
-                   locacao.getUsuario().setNome(scanner.next());
-                   System.out.println("Digite o seu código:");
-                   locacao.getUsuario().setCodigo(scanner.next());
-                   System.out.println("Digite o seu CPF:");
-                   locacao.getUsuario().setCpf(scanner.next());
-                   System.out.println("Digite a sua rua:");
-                   locacao.getUsuario().getEndereco().setRua(scanner.next());
-                   System.out.println("Digite o seu bairro:");
-                   locacao.getUsuario().getEndereco().setBairro(scanner.next());
-                   System.out.println("Digite o número da sua casa:");
-                   locacao.getUsuario().getEndereco().setNumero(scanner.nextInt());
-                   break;
-               case 3:
-                   System.out.println("::::::::::::::::::Bem vindo ao menu livro::::::::::::::::::::::");
-                   System.out.println("Digite o título do livro:");
-                   locacao.getLivro().setTitulo(scanner.next());
-                   System.out.println("Digite o gênero do livro:");
-                   locacao.getLivro().setGenero(scanner.next());
-                   System.out.println("Digite o nome do autor");
-                   locacao.getLivro().setAutor(scanner.next());
-                   break;
-               case 4:
-                   System.out.println("Bem vindo ao menu cadastro");
-                   break;
-               case 5:
-                   System.out.println("Bem vindo ao menu locação");
-                   break;
-               case 0:
-                   break;
-               default:
-                   System.out.println("Opção inválida");
+                    System.out.println("Digite seu nome:");
+                    locacao.getUsuario().setNome(scanner.next());
+                    System.out.println("Digite o seu código:");
+                    locacao.getUsuario().setCodigo(scanner.next());
+                    System.out.println("Digite o seu CPF:");
+                    locacao.getUsuario().setCpf(scanner.next());
+                    System.out.println("Digite a sua rua:");
+                    locacao.getUsuario().getEndereco().setRua(scanner.next());
+                    System.out.println("Digite o seu bairro:");
+                    locacao.getUsuario().getEndereco().setBairro(scanner.next());
+                    System.out.println("Digite o número da sua casa:");
+                    locacao.getUsuario().getEndereco().setNumero(scanner.nextInt());
+                    break;
+                case 3:
+                    System.out.println("::::::::::::::::::Bem vindo ao menu livro::::::::::::::::::::::");
+                    System.out.println("Digite o título do livro:");
+                    locacao.getLivro().setTitulo(scanner.next());
+                    System.out.println("Digite o gênero do livro:");
+                    locacao.getLivro().setGenero(scanner.next());
+                    System.out.println("Digite o nome do autor");
+                    locacao.getLivro().setAutor(scanner.next());
+                    break;
 
-           }
-        }while (opcao !=9);
+                switch (opcao) {
+                    case 4:
+                        System.out.print("Selecione o cadastro na qual o mesmo queira acessar: \n" +
+                                " 1 - Cadastro do Funcionário: \n" +
+                                " 2 - Cadastro do Usuário: \n" +
+                                " 3 - Cadastro do Livro: \n" +
+                                " 9 _ Voltar ao menu principal: \n" +
+                                "Digite a opção desejada: ");
+                        opcao = scanner.nextInt();
+                    case 5:
+                        System.out.println("Bem vindo ao menu locação");
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Opção inválida");
+
+                }
+
+            }
+        } while (opcao != 0);
+
     }
-}
